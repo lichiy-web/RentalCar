5954f4a329efc88d59350708a33c0735f3b20006
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-06-21 21:25:37 +0300

    feature/add-handleResetError-to-NavLinks

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,36 @@
 import { NavLink } from 'react-router-dom';
 import css from './Navigation.module.css';
 import clsx from 'clsx';
+import { useDispatch } from 'react-redux';
+import { resetError } from '../../redux/catalog/slice';
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
 };
 
 const Navigation = () => {
+  const dispatch = useDispatch();
+  const handleResetError = () => dispatch(resetError());
   return (
     <nav className={css.navigation}>
       <ul className={css.navList}>
         <li className={css.navItem}>
-          <NavLink to="/" className={buildLinkClass} end>
+          <NavLink
+            to="/"
+            className={buildLinkClass}
+            end
+            onClick={handleResetError}
+          >
             Home
           </NavLink>
         </li>
         <li className={css.navItem}>
-          <NavLink to="catalog" className={buildLinkClass} end>
+          <NavLink
+            to="catalog"
+            className={buildLinkClass}
+            end
+            onClick={handleResetError}
+          >
             Catalog
           </NavLink>
         </li>
